5b2b781e12655c5bcd9fbf790731c515bcf9bc1b
diak <diak@mesterlevel.lo>
2026-01-20 08:33:48 +0100

    folders/files organising

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import '../css/Home.css'
+
+export default function Home() {
+  return (
+    <>
+    </>
+  )
+}
+
